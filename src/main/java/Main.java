8010e818ce08c7d6ffffe1bee7acb0a665d6ede0
tipsy <davidaase@hotmail.com>
2015-08-25 17:46:39 +0200

    Add example

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
+}
